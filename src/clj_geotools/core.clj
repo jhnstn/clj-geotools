0236d9e903881ea60c163043cabe1952f3046d89
Jason Johnston <johnston@nationbuilder.com>
2013-11-20 18:12:52 -0800

    first commit

---
+++ src/clj_geotools/core.clj
@@ -0,0 +1,6 @@
+(ns clj-geotools.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_geotools/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-geotools.core-test
+  (:use clojure.test
+        clj-geotools.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
